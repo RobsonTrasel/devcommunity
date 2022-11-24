50b22befe25edfbadaeeac4e7cb53ca88b6c966e
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 20:11:35 -0300

    feat: subjects migrations created

---
+++ database/migrations/2022_11_24_230811_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('slug', 100);
+            $table->unsignedInteger('my_class_id')->unique();
+            $table->unsignedInteger('teacher_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
