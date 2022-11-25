b7d31312a79203fa488ebf247f5bf03fe96f5182
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:11 -0300

    chore: model payment record setup done

--- app/Models/PaymentRecord.php
+++ app/Models/PaymentRecord.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class PaymentRecord extends Model
 {
     use HasFactory;
+    protected $fillable =['student_id', 'payment_id', 'amt_paid', 'year', 'paid', 'balance', 'ref_no'];
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function receipt()
+    {
+        return $this->hasMany(Receipt::class, 'pr_id');
+    }
 }
