c93a5b9e24216420caa4117245f7e836ef1509b5
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:11:38 -0300

    fix: sections table modified field

--- database/migrations/2022_11_24_230747_create_sections_table.php
+++ database/migrations/2022_11_24_230747_create_sections_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
-            $table->unsignedInteger('my_class_id')->unique();
-            $table->unsignedInteger('teacher_id')->nullable();
+            $table->unsignedBigInteger('my_class_id')->unique();
+            $table->unsignedBigInteger('teacher_id')->nullable();
             $table->tinyInteger('active')->default(0);
             $table->timestamps();
         });
