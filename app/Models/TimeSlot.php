eef0564adb8517b09a45885db37cd158291893e2
RobsonTrasel <robson.trasel@gmail.com>
2022-11-25 00:00:12 -0300

    chore: model time slot setup done

--- app/Models/TimeSlot.php
+++ app/Models/TimeSlot.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class TimeSlot extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['ttr_id', 'timestamp_from', 'timestamp_to', 'full', 'time_from', 'time_to', 'hour_from', 'min_from', 'meridian_from', 'hour_to', 'min_to', 'meridian_to'];
+
+    public function tt_record()
+    {
+        return $this->belongsTo(TimeTableRecord::class, 'ttr_id');
+    }
 }
