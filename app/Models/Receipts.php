5996c3df82b635216037932f30ccd9c5d3d0a1ab
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:33 -0300

    chore: model receipts setup done

--- app/Models/Receipts.php
+++ app/Models/Receipts.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Receipts extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['pr_id', 'year', 'balance', 'amt_paid'];
+
+    public function pr()
+    {
+        return $this->belongsTo(PaymentRecord::class, 'pr_id');
+    }
 }
