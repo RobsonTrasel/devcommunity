5b7ebf52af1f801a44ee89fd10cba445fccfc040
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:14:40 -0300

    feat: payments records migration added

---
+++ database/migrations/2022_11_24_233005_create_payment_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_records', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('payment_id');
+            $table->unsignedBigInteger('student_id');
+            $table->string('ref_no', 100)->unique()->nullable();
+            $table->integer('amt_paid')->nullable();
+            $table->integer('balance')->nullable();
+            $table->tinyInteger('paid')->default(0);
+            $table->string('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_records');
+    }
+};
