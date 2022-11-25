85f46adaa732ee4abace91a8c374ca3cae55fc96
RobsonTrasel <robson.trasel@gmail.com>
2022-11-25 00:00:26 -0300

    chore: model time table record setup done

--- app/Models/TimeTableRecord.php
+++ app/Models/TimeTableRecord.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class TimeTableRecord extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'my_class_id', 'exam_id', 'year'];
+
+    public function my_class()
+    {
+        return $this->belongsTo(MyClass::class);
+    }
+
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class);
+    }
 }
