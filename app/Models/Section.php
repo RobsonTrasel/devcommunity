31ffdc9d9ece70d08975d8466787ca5f369090cc
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:38 -0300

    chore: model sections setup done

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Section extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'my_class_id', 'active', 'teacher_id'];
+
+    public function my_class()
+    {
+        return $this->belongsTo(MyClass::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function student_record()
+    {
+        return $this->hasMany(StudentRecord::class);
+    }
 }
