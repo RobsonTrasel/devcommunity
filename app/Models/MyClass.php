32374e9434464510931795037bec30bb8e24ec65
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:58:41 -0300

    chore: model my class setup done

--- app/Models/MyClass.php
+++ app/Models/MyClass.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class MyClass extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'class_type_id'];
+
+    public function section()
+    {
+        return $this->hasMany(Section::class);
+    }
+
+    public function class_type()
+    {
+        return $this->belongsTo(ClassType::class);
+    }
+
+    public function student_record()
+    {
+        return $this->hasMany(StudentRecord::class);
+    }
 }
