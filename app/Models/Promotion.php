c294cfc0bbd37d43f0459d54c8a2796ef8fb5b3d
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:28 -0300

    chore: model promotion setup done

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Promotion extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['from_class', 'from_section', 'to_class', 'to_section', 'grad', 'student_id', 'from_session', 'to_session', 'status'];
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function fc()
+    {
+        return $this->belongsTo(MyClass::class, 'from_class');
+    }
+
+    public function fs()
+    {
+        return $this->belongsTo(Section::class, 'from_section');
+    }
+
+    public function ts()
+    {
+        return $this->belongsTo(Section::class, 'to_section');
+    }
+
+    public function tc()
+    {
+        return $this->belongsTo(MyClass::class, 'to_class');
+    }
 }
