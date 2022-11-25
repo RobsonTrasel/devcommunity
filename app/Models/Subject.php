213e3824a20a9d6b2e373c1eda1e117d46433359
RobsonTrasel <robson.trasel@gmail.com>
2022-11-25 00:00:06 -0300

    chore: model subject setup done

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Subject extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'my_class_id', 'teacher_id', 'slug'];
+
+    public function my_class()
+    {
+        return $this->belongsTo(MyClass::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
 }
