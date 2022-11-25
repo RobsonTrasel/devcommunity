563485b826c1474c3c48aca1df3b102ebac0e0d9
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:51 -0300

    chore: model staffrecord setup done

--- app/Models/StaffRecord.php
+++ app/Models/StaffRecord.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class StaffRecord extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['code', 'emp_date', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
