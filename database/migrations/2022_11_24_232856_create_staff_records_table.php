0428b4fcc37b0aaeff3254706b43d969c782a110
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:14:22 -0300

    feat: staffs migration added

---
+++ database/migrations/2022_11_24_232856_create_staff_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff_records', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code', 100)->nullable()->unique();
+            $table->string('emp_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff_records');
+    }
+};
