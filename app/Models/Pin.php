ff31e853258282718b7a3730d458ffd955b0f273
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:17 -0300

    chore: model pin setup done

--- app/Models/Pin.php
+++ app/Models/Pin.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Pin extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['code', 'user_id', 'student_id', 'times_used', 'used'];
+
+    public function user($foreign = NULL)
+    {
+        return $this->belongsTo(User::class, $foreign);
+    }
+
+    public function student()
+    {
+        return $this->user('student_id');
+    }
+
 }
