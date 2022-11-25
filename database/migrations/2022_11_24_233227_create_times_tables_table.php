842d5a810b5edbc9992c90b542137b1a3bd891a5
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:14:52 -0300

    feat: times migration added

---
+++ database/migrations/2022_11_24_233227_create_times_tables_table.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_table_records', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->unsignedBigInteger('my_class_id')->unique();
+            $table->unsignedBigInteger('exam_id')->nullable()->unique();
+            $table->string('year', 100)->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ttr_id');
+            $table->tinyInteger('hour_from');
+            $table->string('min_from', 2);
+            $table->string('meridian_from', 2);
+            $table->tinyInteger('hour_to');
+            $table->string('min_to', 2);
+            $table->string('meridian_to', 2);
+            $table->string('time_from', 100);
+            $table->string('time_to', 100);
+            $table->string('timestamp_from', 50);
+            $table->string('timestamp_to', 50);
+            $table->string('full', 100);
+            $table->timestamps();
+
+            $table->unique(['timestamp_from', 'timestamp_to', 'ttr_id']);
+
+        });
+
+        Schema::create('time_tables', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ttr_id');
+            $table->unsignedBigInteger('ts_id');
+            $table->unsignedBigInteger('subject_id')->nullable();
+            $table->string('exam_date', 50)->nullable();
+            $table->string('timestamp_from', 100);
+            $table->string('timestamp_to', 100);
+            $table->string('day', 50)->nullable();
+            $table->tinyInteger('day_num')->nullable();
+            $table->timestamps();
+
+            $table->unique(['ttr_id', 'ts_id', 'day']);
+            $table->unique(['ttr_id', 'ts_id', 'exam_date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times_tables');
+        Schema::dropIfExists('times_tables');
+        Schema::dropIfExists('times_tables');
+    }
+};
