452812104e45df74b7372f3cb769d1e2a542607d
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:59:02 -0300

    chore: model payment setup done

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'amount', 'my_class_id', 'description', 'year', 'ref_no'];
+
+    public function my_class()
+    {
+        return $this->belongsTo(MyClass::class);
+    }
 }
