2712441dd80ce74eb0bc18646a5b6a304a3593cc
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:11:08 -0300

    fix: lgas table modified field

---
+++ database/migrations/2022_11_24_232759_create_book_request_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('returned')->default(0);
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_request');
+    }
+};
