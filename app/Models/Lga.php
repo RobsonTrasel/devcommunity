572d22a48c35fa85552d7d8766a6e14d84abf39e
RobsonTrasel <robson.trasel@gmail.com>
2022-11-24 23:37:06 -0300

    feat: models added

---
+++ app/Models/Lga.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lga extends Model
+{
+    use HasFactory;
+
+    public function ministry()
+    {
+        // return $this->hasMany(Ministry::class);
+    }
+}
